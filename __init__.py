8ff1406bd041470c84ff2ce17b2f7c058b369205
GabrielAmare <gabriel.amare.31@gmail.com>
2022-12-01 12:17:15 +0100

    removed previous implementation

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .TypeCode import TypeCode
-from .EntityCode import EntityCode
-from .TimeCode import TimeCode
-from .VerbCode import VerbCode
-from .ConjCode import ConjCode
-from .WordCode import WordCode
